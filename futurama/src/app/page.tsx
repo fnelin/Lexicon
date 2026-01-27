d9ce7d1e194c349eb69f6faa785a35ac773c7d6a
Fredrik.N <fredrik.nelin@outlook.com>
2026-01-27 20:11:19 +0100

    Added hero to landingpage

--- futurama/src/app/page.tsx
+++ futurama/src/app/page.tsx
@@ -2,36 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <main className="bg-linear-145 from-blue-900 from-75% via-blue-700 via-85% to-sky-500 to-99%">
+      <section className="px-4 grid lg:grid-cols-2">
+        <div className="my-2">
+          <span className="text-2xl text-neutral-50">About time!</span>
+          <h1 className="text-5xl leading-tight text-pretty font-bold text-neutral-100">
+            Futurama <span className="line-through">might be</span> is back!
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            {" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+          <p className="text-neutral-100 text-pretty">Matt Groening's hilarious animated series about a pizza delivery man named Fry who is accidentally cryogenically frozen in 1999 and wakes up 1000 years later to a brave new world - that in some ways hasn't progressed much beyond the 20th century.</p> 
+        </div> 
+       <Image src="/futurama_hero.jpeg" 
+          alt="" 
+          width={1920} height={1080} 
+          loading="eager"
+          className="border-6 border-amber-50 rounded-md  max-w-9/10"
+        />
+      
+      </section>
+      <section>Card...</section>
+    </main>
+
   );
 }
--- futurama/src/components/main-navigation.tsx
+++ futurama/src/components/main-navigation.tsx
@@ -1,19 +1,28 @@
 import Link from "next/link";
 
+const menu = [
+  {title:"Home", href:"/"},
+  {title:"About", href:"/about"},
+  {title:"Contact", href:"/contact"},
+]
+
+const renderMenu = () => {
+  return menu.map((item, index)=>
+  <li key={index}>
+    <Link className="hover:text-sky-700 transition-colors" href={item.href}>
+    {item.title}
+    </Link>
+  </li>)
+}
+
 export default function MainNavigation() {
   return (
-    <div className="flex justify-around p-2 border-b sticky top-0 bg-neutral-100/60 backdrop-blur-sm font-bold">
-      <Link href="/">Logga</Link>
-      <ul className="flex gap-6">
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-             <li>
-          <Link href="/contact">Contact</Link>
-        </li>
+    <div className="flex items-center justify-around p-2 border-b sticky top-0 bg-neutral-100/60 backdrop-blur-sm font-bold">
+      <Link href="/"><img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Futurama_1999_logo.svg" width="100" height="50"/></Link>
+      <ul className="flex gap-8">
+      {
+       renderMenu()
+      }
       </ul>
     </div>
   );
