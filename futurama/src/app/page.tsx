3e53e0335c12349464569ae662a692b166a0a314
Fredrik.N <fredrik.nelin@outlook.com>
2026-02-05 21:51:34 +0100

    adjusted simple filter for amount of characters fetched

--- futurama/src/app/page.tsx
+++ futurama/src/app/page.tsx
@@ -1,17 +1,19 @@
+import Link from "next/link"
 import CardGrid from "./ui/card-grid"
 import HeroBanner from "./ui/hero-banner";
 import { GridItemBase } from "@/components/grid-items"
 import getData from "@/components/load-data"
 
-export default async function Home() {
-
-  const apiCharacters: GridItemBase[] = await getData("https://futuramaapi.com/api/characters");
+export default async function Home(params: PageProps<"/">) {
+  const { limit = "12" } = await params.searchParams
+  const limitNumber = limit;
+  const apiCharacters: GridItemBase[] = await getData(`https://futuramaapi.com/api/characters?size=${limitNumber}`);
 
   return (
     <main className="scroll-smooth bg-blue-50">
       <HeroBanner />
       <hr className="" />
       <CardGrid gridName="Characters" gridItems={apiCharacters} />
-    </main>
+    </main >
   );
 }
--- futurama/src/app/ui/card-grid.tsx
+++ futurama/src/app/ui/card-grid.tsx
@@ -1,22 +1,24 @@
+import Link from "next/link"
 import { GridItemBase } from "@/components/grid-items";
 import GridCard from "./cards";
 
 
-export default function CardGrid({gridName, gridItems}: {gridName:string, gridItems: GridItemBase[]}){
+export default function CardGrid({ gridName, gridItems }: { gridName: string, gridItems: GridItemBase[] }) {
 
-  return(
+  return (
     <section className="container mx-auto px-4 py-4 space-y-4">
       <h2 className="text-2xl font-bold font-josefin text-center">{gridName}</h2>
+      <span className="">Limit:<Link href="./?limit=10" className="font-bold mr-2">10</Link><Link href="./?limit=20" className="font-bold">20</Link></span>
       <ul className="grid grid-cols-[repeat(auto-fill,minmax(20ch,1fr))] gap-2 ">
         {
-        gridItems.map((gI)=>(
-        <li key={gI.id} className="p-2 border rounded-t-2xl overflow-hidden duration-500 ease-in-out bg-blue-700
+          gridItems.map((gI) => (
+            <li key={gI.id} className="p-2 border rounded-t-2xl overflow-hidden duration-500 ease-in-out bg-blue-700
                                   hover:-translate-1.5 hover:shadow-2xl hover:bg-blue-700/80">
-          
-          <GridCard id={gI.id} name={gI.name} imgStr={gI.image} itemStatus={gI.status} page={gridName}/>
 
-        </li>
-        ))}
+              <GridCard id={gI.id} name={gI.name} imgStr={gI.image} itemStatus={gI.status} page={gridName} />
+
+            </li>
+          ))}
       </ul>
     </section>
 
